e769f8a8824d8168f760f9af6951efdae4e97ecf
Jemmy Wang <wangzihao12345678@126.com>
2024-11-13 21:21:06 -0500

    SAT CDCL library design

---
+++ smt-solver/backend/lib/sat_cdcl/assignment.ml
@@ -0,0 +1,8 @@
+type t
+type value = Positive | Negative | Absent
+
+let create = failwith "unimplemented"
+
+let set = failwith "unimplemented"
+
+let get = failwith "unimplemented"
---
+++ smt-solver/backend/lib/sat_cdcl/literal.ml
@@ -0,0 +1,5 @@
+type t
+
+let create : Variable.t -> bool -> t = failwith "Not implemented"
+
+let value : t -> Assignment.t -> Assignment.value = failwith "Not implemented"
