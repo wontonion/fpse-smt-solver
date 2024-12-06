6d7d20b6f129e1f3b4afeaa4c4016b8ed07d4b45
Jemmy Wang <wangzihao12345678@126.com>
2024-12-06 01:45:13 -0500

    Initial SAT solver

--- solver/lib/cdcl/solver.ml
+++ solver/lib/cdcl/solver.ml
@@ -1,7 +1,22 @@
 open Core
 
-type status = SATIISFIED | UNSATISFIED | UNIT | UNRESOLVED
+type status = SATISFIED | UNSATISFIED | UNIT | UNRESOLVED
 
+[@@@warning "-32"]
+
+module type Heuristic = sig
+  val pick_branching_variable : Formula.t -> Assignment.t -> int * bool
+  (** [pick_branching_variable f a] picks a branching variable in formula [f] with assignment [a] *)
+end
+
+module type Solver = sig
+  val cdcl_solve : Formula.t -> Assignment.t option
+  (** [cdcl_solve f] solves formula [f] using the CDCL algorithm *)
+end
+
+module Make (H : Heuristic) : Solver = struct
+  let cdcl_solve = failwith "unimplemented"
+end
 
 let all_variables_assigned (f : Formula.t) (a : Assignment.t) : bool =
   List.for_all
@@ -18,15 +33,15 @@ let pick_branching_variable (f : Formula.t) (a : Assignment.t) : int * bool =
     | v :: vs -> (
         match Assignment.value a (Literal.create v false) with
         | Some _ -> loop vs
-        | None -> (v, false))
+        | None ->
+            let b = Random.bool () in
+            printf "Picking variable %d with value %b\n" v b;
+            (v, b))
   in
   loop (Set.to_list (Formula.variables f))
 
 let backtrack (a : Assignment.t) (dl : int) : Assignment.t =
-  {
-    values = Map.filter ~f:(fun (d : Assignment.d) -> d.dl < dl) a.values;
-    dl;
-  }
+  { values = Map.filter ~f:(fun (d : Assignment.d) -> d.dl <= dl) a.values; dl }
 
 let clause_status (c : Clause.t) (a : Assignment.t) : status =
   let res =
@@ -40,122 +55,105 @@ let clause_status (c : Clause.t) (a : Assignment.t) : status =
       ~finish:Fn.id
   in
   match res with
-  | num_true, _, _ when num_true > 0 -> SATIISFIED
+  | num_true, _, _ when num_true > 0 -> SATISFIED
   | _, _, num_unassigned when num_unassigned = 1 -> UNIT
   | num_true, _, num_unassigned when num_true = 0 && num_unassigned = 0 ->
       UNSATISFIED
   | _ -> UNRESOLVED
 
 let unit_propagation (f : Formula.t) (a : Assignment.t) :
-    Assignment.t * Clause.t option =
-  let unit_propagation_once (f : Formula.t) (a : Assignment.t) :
-      Assignment.t * Clause.t option * bool =
-    List.fold_until (Formula.clauses f) ~init:(a, None, true)
+    Assignment.t * [ `NoConflict | `Conflict of Clause.t ] =
+  let rec unit_propagation_once (f : Formula.t) (a : Assignment.t) :
+      Assignment.t * [ `NoConflict | `Conflict of Clause.t ] =
+    List.fold_until (Formula.clauses f) ~init:(a, `NoConflict, true)
       ~f:(fun (a, conflict, finish) c ->
         match clause_status c a with
-        | SATIISFIED | UNRESOLVED -> Continue (a, conflict, finish)
-        | UNSATISFIED -> Stop (a, Some c, finish)
+        | SATISFIED | UNRESOLVED -> Continue (a, conflict, finish)
+        | UNSATISFIED -> Stop (a, `Conflict c, finish)
         | UNIT ->
             let l =
-              List.find_exn (Clause.literals c) ~f:(fun l -> Option.is_none @@ Assignment.value a l)
+              List.find_exn (Clause.literals c) ~f:(fun l ->
+                  Option.is_none @@ Assignment.value a l)
             in
             Continue
               ( Assignment.assign a l.variable (not l.negation) (Some c),
                 conflict,
                 false ))
       ~finish:Fn.id
+    |> fun (a, conflict, finish) ->
+    if finish then (a, conflict) else unit_propagation_once f a
   in
-  let rec loop (a : Assignment.t) : Assignment.t * Clause.t option =
-    let a', conflict, finish = unit_propagation_once f a in
-    if finish then (a', conflict) else loop a'
-  in
-  loop a
+  unit_propagation_once f a
 
 let resolve (c1 : Clause.t) (c2 : Clause.t) (v : int) : Clause.t =
   let lits1 = List.filter (Clause.literals c1) ~f:(fun l -> l.variable <> v) in
   let lits2 = List.filter (Clause.literals c2) ~f:(fun l -> l.variable <> v) in
   Clause.create (lits1 @ lits2)
 
-let conflict_analysis_once (c : Clause.t) (a : Assignment.t) : Clause.t * bool =
-  let literals =
-    List.filter (Clause.literals c) ~f:(fun l ->
-        match Assignment.dl a l.variable with
-        | Some dl -> dl = a.dl
-        | None -> false)
+let conflict_analysis (c : Clause.t) (a : Assignment.t) : int * Clause.t =
+  let rec conflict_analysis_once (c : Clause.t) (a : Assignment.t) : Clause.t =
+    let literals =
+      List.filter (Clause.literals c) ~f:(fun l ->
+          match Assignment.dl a l.variable with
+          | Some dl -> dl = a.dl
+          | None -> false)
+    in
+    match List.length literals with
+    | 1 -> c
+    | _ ->
+        let literal =
+          List.find_exn literals ~f:(fun l ->
+              match Assignment.antecedent a l.variable with
+              | Some _ -> true
+              | None -> false)
+        in
+        let antecedent =
+          Option.value_exn (Assignment.antecedent a literal.variable)
+        in
+        conflict_analysis_once (resolve c antecedent literal.variable) a
   in
-  match List.length literals with
-  | 1 -> (c, true)
-  | _ ->
-      let literals =
-        List.filter literals ~f:(fun l ->
-            match Assignment.antecedent a l.variable with
-            | Some _ -> true
-            | None -> false)
-      in
-      let literal = List.hd_exn literals in
-      let antecedent =
-        Option.value_exn (Assignment.antecedent a literal.variable)
-      in
-      (resolve c antecedent literal.variable, false)
-
-let conflict_analysis (c : Clause.t) (a : Assignment.t) : int * Clause.t option
-    =
-  match a.dl with
-  | 0 -> (-1, None)
-  | _ -> (
-      let rec loop (c : Clause.t) (a : Assignment.t) : Clause.t =
-        let c', finish = conflict_analysis_once c a in
-        if finish then c' else loop c' a
-      in
-      let clause = loop c a in
-      let decision_levels =
-        List.map (Clause.literals clause) ~f:(fun l ->
-            match Assignment.dl a l.variable with
-            | Some dl -> dl
-            | None -> failwith "variable not in assignment")
-      in
-      match List.length decision_levels with
-      | 0 | 1 -> (List.hd_exn decision_levels, Some clause)
-      | _ ->
-          let max_dl = List.max_elt decision_levels ~compare:Int.compare in
-          let decision_levels =
-            List.filter decision_levels ~f:(fun dl ->
-                dl < Option.value_exn max_dl)
-          in
-          ( List.max_elt decision_levels ~compare:Int.compare |> Option.value_exn,
-            Some clause ))
-
-type choice = Backtrack of int | Continue of Assignment.t
-
-let rec cdcl_solve_once (formula : Formula.t) (assignment : Assignment.t) :
-    Formula.t * choice =
-  match all_variables_assigned formula assignment with
-  | true -> (formula, Continue assignment)
-  | false -> (
-      let var, value = pick_branching_variable formula assignment in
-      let assignment =
-        Assignment.assign
-          { assignment with dl = assignment.dl + 1 }
-          var value None
-      in
-      let clause = unit_propagation formula assignment in
-      match clause with
-      | assignment, None -> cdcl_solve_once formula assignment
-      | assignment, Some clause ->
-          let dl, learnt_clause = conflict_analysis clause assignment in
-          let formula =
-            match learnt_clause with
-            | Some learnt_clause -> Formula.add_clause formula learnt_clause
-            | None -> formula
-          in
-          (formula, Backtrack dl))
+  if a.dl <= 0 then failwith "Invalid decision level"
+  else
+    let clause = conflict_analysis_once c a in
+    let decision_levels =
+      List.dedup_and_sort ~compare:(fun x y -> Int.compare y x)
+      @@ List.map (Clause.literals clause) ~f:(fun l ->
+             match Assignment.dl a l.variable with
+             | Some dl -> dl
+             | None -> failwith "variable not in assignment")
+    in
+    match List.length decision_levels with
+    | 0 | 1 -> (0, clause)
+    | _ -> (List.nth_exn decision_levels 1, clause)
 
 let cdcl_solve (formula : Formula.t) : Assignment.t option =
-  let assignment = Assignment.empty in
-  let clause = unit_propagation formula assignment in
-  match clause with
-  | _, Some _ -> None
-  | _, None -> (
-      cdcl_solve_once formula assignment |> snd |> function
-      | Backtrack dl -> Some (backtrack assignment dl)
-      | Continue assignment -> Some assignment)
+  let rec learn_clause (f : Formula.t) (a : Assignment.t) :
+      Formula.t * Assignment.t =
+    let assignment, conflict = unit_propagation f a in
+    match conflict with
+    | `NoConflict -> (f, assignment)
+    | `Conflict clause ->
+        let dl, learnt = conflict_analysis clause assignment in
+        let formula' = Formula.add_clause f learnt in
+        let assignment' = backtrack assignment dl in
+        learn_clause formula' assignment'
+  in
+  let rec cdcl_solve_once (formula : Formula.t) (assignment : Assignment.t) :
+      Assignment.t option =
+    match all_variables_assigned formula assignment with
+    | true -> Some assignment
+    | false ->
+        let variable, value = pick_branching_variable formula assignment in
+        let assignment' =
+          Assignment.assign
+            { assignment with dl = assignment.dl + 1 }
+            variable value None
+        in
+        let formula', assignment'' = learn_clause formula assignment' in
+        cdcl_solve_once formula' assignment''
+  in
+  let a = Assignment.empty in
+  let assignment, conflict = unit_propagation formula a in
+  match conflict with
+  | `Conflict _ -> None
+  | `NoConflict -> cdcl_solve_once formula assignment
--- solver/test/cdcl/solver_tests.ml
+++ solver/test/cdcl/solver_tests.ml
@@ -1,3 +1,270 @@
 open OUnit2
 
-let series = "Solver tests" >::: []
+[@@@warning "-26"]
+
+let test_all_variables_assigned _ =
+  let l1 = Cdcl.Literal.create 1 false in
+  let l1' = Cdcl.Literal.create 1 true in
+  let l2 = Cdcl.Literal.create 2 false in
+  let l2' = Cdcl.Literal.create 2 true in
+  let l3 = Cdcl.Literal.create 3 false in
+  let l3' = Cdcl.Literal.create 3 true in
+  let c1 = Cdcl.Clause.create [ l1 ] in
+  let c2 = Cdcl.Clause.create [ l2'; l3' ] in
+  let f = Cdcl.Formula.create [ c1; c2 ] in
+  assert_equal false
+    (Cdcl.Solver.all_variables_assigned f Cdcl.Assignment.empty);
+
+  let a = Cdcl.Assignment.assign Cdcl.Assignment.empty 1 true None in
+  assert_equal false (Cdcl.Solver.all_variables_assigned f a);
+
+  let a' = Cdcl.Assignment.assign a 2 true None in
+  assert_equal false (Cdcl.Solver.all_variables_assigned f a');
+
+  let a'' = Cdcl.Assignment.assign a' 3 true None in
+  assert_equal true (Cdcl.Solver.all_variables_assigned f a'')
+
+let test_clause_status _ =
+  let l1 = Cdcl.Literal.create 1 false in
+  let l1' = Cdcl.Literal.create 1 true in
+  let l2 = Cdcl.Literal.create 2 false in
+  let l2' = Cdcl.Literal.create 2 true in
+  let l3 = Cdcl.Literal.create 3 false in
+  let l3' = Cdcl.Literal.create 3 true in
+  let c0 = Cdcl.Clause.create [ l1 ] in
+  let c1 = Cdcl.Clause.create [ l2'; l3' ] in
+  let c2 = Cdcl.Clause.create [ l1'; l2 ] in
+  assert_equal Cdcl.Solver.UNIT
+    (Cdcl.Solver.clause_status c0 Cdcl.Assignment.empty);
+  assert_equal Cdcl.Solver.UNRESOLVED
+    (Cdcl.Solver.clause_status c1 Cdcl.Assignment.empty);
+  assert_equal Cdcl.Solver.UNRESOLVED
+    (Cdcl.Solver.clause_status c2 Cdcl.Assignment.empty);
+
+  let a = Cdcl.Assignment.assign Cdcl.Assignment.empty 1 true None in
+  assert_equal Cdcl.Solver.SATISFIED (Cdcl.Solver.clause_status c0 a);
+  assert_equal Cdcl.Solver.UNRESOLVED (Cdcl.Solver.clause_status c1 a);
+  assert_equal Cdcl.Solver.UNIT (Cdcl.Solver.clause_status c2 a)
+
+let test_unit_propagation_1 _ =
+  let l1 = Cdcl.Literal.create 1 false in
+  let l1' = Cdcl.Literal.create 1 true in
+  let l2 = Cdcl.Literal.create 2 false in
+  let l2' = Cdcl.Literal.create 2 true in
+  let l3 = Cdcl.Literal.create 3 false in
+  let l3' = Cdcl.Literal.create 3 true in
+  let c0 = Cdcl.Clause.create [ l2'; l3' ] in
+  let c1 = Cdcl.Clause.create [ l1'; l2 ] in
+  let c2 = Cdcl.Clause.create [ l1 ] in
+  let f = Cdcl.Formula.create [ c0; c1; c2 ] in
+  let a = Cdcl.Assignment.empty in
+  let a', conflict = Cdcl.Solver.unit_propagation f a in
+  assert_equal `NoConflict conflict;
+  assert_equal (Some true)
+    (Cdcl.Assignment.value a' @@ Cdcl.Literal.create 1 false);
+  assert_equal (Some true)
+    (Cdcl.Assignment.value a' @@ Cdcl.Literal.create 2 false);
+  assert_equal (Some false)
+    (Cdcl.Assignment.value a' @@ Cdcl.Literal.create 3 false)
+
+let test_unit_propagation_2 _ =
+  let l1 = Cdcl.Literal.create 1 false in
+  let l1' = Cdcl.Literal.create 1 true in
+  let l2 = Cdcl.Literal.create 2 false in
+  let l2' = Cdcl.Literal.create 2 true in
+  let l3 = Cdcl.Literal.create 3 false in
+  let l3' = Cdcl.Literal.create 3 true in
+  let l4 = Cdcl.Literal.create 4 false in
+  let l4' = Cdcl.Literal.create 4 true in
+  let c0 = Cdcl.Clause.create [ l1'; l3'; l4 ] in
+  let c1 = Cdcl.Clause.create [ l1'; l2 ] in
+  let c2 = Cdcl.Clause.create [ l1 ] in
+  let f = Cdcl.Formula.create [ c0; c1; c2 ] in
+  let a = Cdcl.Assignment.empty in
+  let a', conflict = Cdcl.Solver.unit_propagation f a in
+  assert_equal `NoConflict conflict;
+  assert_equal (Some true)
+    (Cdcl.Assignment.value a' @@ Cdcl.Literal.create 1 false);
+  assert_equal (Some true)
+    (Cdcl.Assignment.value a' @@ Cdcl.Literal.create 2 false);
+  assert_equal None (Cdcl.Assignment.value a' @@ Cdcl.Literal.create 3 false);
+  assert_equal None (Cdcl.Assignment.value a' @@ Cdcl.Literal.create 4 false)
+
+let test_resolve _ =
+  let l1 = Cdcl.Literal.create 1 false in
+  let l1' = Cdcl.Literal.create 1 true in
+  let l2 = Cdcl.Literal.create 2 false in
+  let l2' = Cdcl.Literal.create 2 true in
+  let l3 = Cdcl.Literal.create 3 false in
+  let l3' = Cdcl.Literal.create 3 true in
+  let l4 = Cdcl.Literal.create 4 false in
+  let l4' = Cdcl.Literal.create 4 true in
+  let c1 = Cdcl.Clause.create [ l1'; l2 ] in
+  let c2 = Cdcl.Clause.create [ l1; l3' ] in
+  let c = Cdcl.Solver.resolve c1 c2 1 in
+  assert_equal [ l3'; l2 ] (Cdcl.Clause.literals c)
+
+let test_conflict_analysis _ =
+  let c1 =
+    Cdcl.Clause.create
+      [
+        Cdcl.Literal.create 2 true;
+        Cdcl.Literal.create 3 true;
+        Cdcl.Literal.create 4 true;
+        Cdcl.Literal.create 5 false;
+      ]
+  in
+  let c2 =
+    Cdcl.Clause.create
+      [
+        Cdcl.Literal.create 1 true;
+        Cdcl.Literal.create 5 true;
+        Cdcl.Literal.create 6 false;
+      ]
+  in
+  let c3 =
+    Cdcl.Clause.create
+      [ Cdcl.Literal.create 5 true; Cdcl.Literal.create 7 false ]
+  in
+  let c4 =
+    Cdcl.Clause.create
+      [
+        Cdcl.Literal.create 1 true;
+        Cdcl.Literal.create 6 true;
+        Cdcl.Literal.create 7 true;
+      ]
+  in
+  let c5 =
+    Cdcl.Clause.create
+      [
+        Cdcl.Literal.create 1 true;
+        Cdcl.Literal.create 2 true;
+        Cdcl.Literal.create 5 false;
+      ]
+  in
+  let c6 =
+    Cdcl.Clause.create
+      [
+        Cdcl.Literal.create 1 true;
+        Cdcl.Literal.create 3 true;
+        Cdcl.Literal.create 5 false;
+      ]
+  in
+  let c7 =
+    Cdcl.Clause.create
+      [
+        Cdcl.Literal.create 1 true;
+        Cdcl.Literal.create 4 true;
+        Cdcl.Literal.create 5 false;
+      ]
+  in
+  let c7 =
+    Cdcl.Clause.create
+      [
+        Cdcl.Literal.create 1 true;
+        Cdcl.Literal.create 2 false;
+        Cdcl.Literal.create 3 false;
+        Cdcl.Literal.create 4 false;
+        Cdcl.Literal.create 5 false;
+        Cdcl.Literal.create 6 true;
+      ]
+  in
+  let f = Cdcl.Formula.create [ c1; c2; c3; c4; c5; c6; c7 ] in
+  let a1 =
+    Cdcl.Assignment.assign { Cdcl.Assignment.empty with dl = 1 } 1 true None
+  in
+  let a2 = Cdcl.Assignment.assign { a1 with dl = 2 } 2 true None in
+  let a, conflict = Cdcl.Solver.unit_propagation f a2 in
+  let dl, learned =
+    match conflict with
+    | `Conflict clause -> Cdcl.Solver.conflict_analysis clause a
+    | `NoConflict -> failwith "Expected a conflict"
+  in
+  assert_equal 1 dl;
+  assert_equal
+    [ Cdcl.Literal.create 5 true; Cdcl.Literal.create 1 true ]
+    (Cdcl.Clause.literals learned)
+
+let test_cdcl_solve _ =
+  let c1 =
+    Cdcl.Clause.create
+      [
+        Cdcl.Literal.create 2 true;
+        Cdcl.Literal.create 3 true;
+        Cdcl.Literal.create 4 true;
+        Cdcl.Literal.create 5 false;
+      ]
+  in
+  let c2 =
+    Cdcl.Clause.create
+      [
+        Cdcl.Literal.create 1 true;
+        Cdcl.Literal.create 5 true;
+        Cdcl.Literal.create 6 false;
+      ]
+  in
+  let c3 =
+    Cdcl.Clause.create
+      [ Cdcl.Literal.create 5 true; Cdcl.Literal.create 7 false ]
+  in
+  let c4 =
+    Cdcl.Clause.create
+      [
+        Cdcl.Literal.create 1 true;
+        Cdcl.Literal.create 6 true;
+        Cdcl.Literal.create 7 true;
+      ]
+  in
+  let c5 =
+    Cdcl.Clause.create
+      [
+        Cdcl.Literal.create 1 true;
+        Cdcl.Literal.create 2 true;
+        Cdcl.Literal.create 5 false;
+      ]
+  in
+  let c6 =
+    Cdcl.Clause.create
+      [
+        Cdcl.Literal.create 1 true;
+        Cdcl.Literal.create 3 true;
+        Cdcl.Literal.create 5 false;
+      ]
+  in
+  let c7 =
+    Cdcl.Clause.create
+      [
+        Cdcl.Literal.create 1 true;
+        Cdcl.Literal.create 4 true;
+        Cdcl.Literal.create 5 false;
+      ]
+  in
+  let c7 =
+    Cdcl.Clause.create
+      [
+        Cdcl.Literal.create 1 true;
+        Cdcl.Literal.create 2 false;
+        Cdcl.Literal.create 3 false;
+        Cdcl.Literal.create 4 false;
+        Cdcl.Literal.create 5 false;
+        Cdcl.Literal.create 6 true;
+      ]
+  in
+  let f = Cdcl.Formula.create [ c1; c2; c3; c4; c5; c6; c7 ] in
+  let assignment = match Cdcl.Solver.cdcl_solve f with
+    | Some a -> a
+    | None -> failwith "Expected a solution"
+  in
+  assert_equal true (Cdcl.Assignment.satisfy assignment f)
+
+let series =
+  "Solver tests"
+  >::: [
+         "Test all_variables_assigned" >:: test_all_variables_assigned;
+         "Test clause_status" >:: test_clause_status;
+         "Test unit_propagation 1" >:: test_unit_propagation_1;
+         "Test unit_propagation 2" >:: test_unit_propagation_2;
+         "Test resolve" >:: test_resolve;
+         "Test conflict_analysis" >:: test_conflict_analysis;
+          "Test cdcl_solve" >:: test_cdcl_solve
+       ]
