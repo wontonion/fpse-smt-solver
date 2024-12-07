f394a4423512bab624e12450c32cf1bac5d4c4c0
Jemmy Wang <wangzihao12345678@126.com>
2024-12-06 21:45:58 -0500

    Add more test CNFs

--- solver/bin/main.ml
+++ solver/bin/main.ml
@@ -1,1 +1,13 @@
-let () = print_endline "Hello, World!"
+open Core
+module RandomSolver = Cdcl.Solver.Make (Cdcl.Heuristic.Random)
+
+let[@landmark] main () =
+  let input = In_channel.input_all In_channel.stdin in
+  let formula = Dimacs.Parser.parse input in
+  match RandomSolver.cdcl_solve formula with
+  | `UNSAT -> print_endline "UNSATISFIABLE"
+  | `SAT assignment ->
+      print_endline "SATISFIABLE";
+      print_endline (Cdcl.Assignment.string_of_t assignment)
+
+let () = main ()
