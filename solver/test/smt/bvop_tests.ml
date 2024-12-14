6cf9e6ed3d15e29fcc1811b5f2dc21e8c54f49f0
Jemmy Wang <wangzihao12345678@126.com>
2024-12-14 01:58:20 -0500

    Add complex tests

--- solver/test/smt/bvop_tests.ml
+++ solver/test/smt/bvop_tests.ml
@@ -122,6 +122,19 @@ let test_mul _ =
   in
   assert_equal 51484 @@ Bitvec.value assignment o.o0
 
+let test_solve_equation _ =
+  let ctx', bv1 = Bvop.op_var ctx in
+  let ctx', bv2 = Bvop.op_mul ctx' { i0 = bv1.o0; i1 = 3 } in
+  let ctx', bv3 = Bvop.op_add ctx' { i0 = bv2.o0; i1 = Bitvec.constant f 4 } in
+  let ctx', bv4 = Bvop.op_xor ctx' { i0 = bv1.o0; i1 = Bitvec.constant f 14 } in
+  let ctx' = Bvop.constraint_eq ctx' { i0 = bv3.o0; i1 = bv4.o0 } in
+  let assignment =
+    match Context.solve ctx' with
+    | `SAT assignment -> assignment
+    | `UNSAT -> assert_failure "UNSAT"
+  in
+  assert_equal true @@ List.mem (Bitvec.value assignment bv1.o0) [ 32771; 3 ]
+
 let series =
   "BitOp tests"
   >::: [
@@ -134,4 +147,5 @@ let series =
          "Test ADD" >:: test_add;
          "Test SHL" >:: test_shl;
          "Test MUL" >:: test_mul;
+         "Test solve equation" >:: test_solve_equation;
        ]
